d90f1273c4783d5303caf115df610de5622f22f5
Akshay Malik <elviawellnessglobal@gmail.com>
2026-01-06 15:32:46 +0530

    Fix routing: set Home as default route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App";
+import App from "./App"; // Product page
+import Home from "./pages/Home"; // Homepage
+
 import Cart from "./pages/Cart";
 import Address from "./pages/Address";
 import Payment from "./pages/Payment";
 import OrderSuccess from "./pages/OrderSuccess";
 
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
-import ProtectedRoute from "./components/ProtectedRoute";
 import About from "./pages/legal/About";
 import Contact from "./pages/legal/Contact";
 import Privacy from "./pages/legal/Privacy";
 import Terms from "./pages/legal/Terms";
 import Refund from "./pages/legal/Refund";
 import Shipping from "./pages/legal/Shipping";
-import Home from "./pages/Home";
 
-
-
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -28,18 +27,19 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <CartProvider>
         <BrowserRouter>
           <Routes>
+            {/* HOME */}
+            <Route path="/" element={<Home />} />
+
+            {/* PRODUCT */}
+            <Route path="/product" element={<App />} />
+
+            {/* LEGAL PAGES */}
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy-policy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
             <Route path="/refund-policy" element={<Refund />} />
             <Route path="/shipping-policy" element={<Shipping />} />
-            {/* HOME / PRODUCT PAGE */}
-            import Home from "./pages/Home";
-
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<App />} />
-
 
             {/* CART (PROTECTED) */}
             <Route
@@ -71,7 +71,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               }
             />
 
-            {/* SUCCESS PAGE */}
+            {/* ORDER SUCCESS */}
             <Route path="/order-success" element={<OrderSuccess />} />
           </Routes>
         </BrowserRouter>
